103835e735a89936658bc572709365b61b8ed5b8
Fode Manca <fomanca@c2r3s11.42porto.com>
2026-05-07 13:35:18 +0100

    learning how 'GET' works and how to search for users with it

--- minha_api/app/main.py
+++ minha_api/app/main.py
@@ -1,24 +1,34 @@
+from pydantic import BaseModel, Field
 from fastapi import FastAPI
 from fastapi import HTTPException
 
 # instancia da app com nome e versao
 app = FastAPI(title="Learning_API", version="1.0.0")
 
+
 # raiz apenas comfirmar que a API esta viva
 @app.get("/")
 def root():
     return {"message": "API online"}
 
+
+class ItemCreate(BaseModel):
+    name: str = Field(min_length=2, max_length=17)
+
+
 # base de dados por agora
 items = [
-    {"id": 1, "nome": "tesoura"},
-    {"id": 2, "nome": "garrafa"},
-    {"id": 3, "nome": "mesa"}
+    {"id": 1, "name": "Tesoura"},
+    {"id": 2, "name": "Garrafa"},
+    {"id": 3, "name": "Mesa de Jantar"},
+    {"id": 4, "name": "PC Gamer"},
+    {"id": 5, "name": "Teclado"}
 ]
+
 
 # endpoint da lista - responde a GET e devolve a lista toda
 @app.get("/items")
-def get_items():
+def read_items():
     return items
 
 
